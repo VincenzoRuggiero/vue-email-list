c610b5165b1661846c7b7b758f7f998cf3baa8bc
VincenzoRuggiero <vincenzoru@live.it>
2022-12-21 16:01:20 +0100

    Add condition that prints emails to DOM only if their number is equal to 10 or more

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ createApp({
     };
   },
   mounted() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((result) => {
-        this.email = result.data.response;
-        this.emailList.push(this.email);
-      });
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+          this.email = result.data.response;
+          this.emailList.push(this.email);
+        });
+    }
   },
 }).mount("#app");
